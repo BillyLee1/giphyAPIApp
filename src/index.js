b804c28bc199f5ae867cedcde62b1eee22c3c509
Billy Lee <codingbillylee@gmail.com>
2023-03-15 20:39:39 -0700

    Add functionality for gif displaying. Add some CCS

--- src/index.js
+++ src/index.js
@@ -4,18 +4,27 @@ import './css/styles.css';
 
 function fetchGiph(userSearch) {
   let request = new XMLHttpRequest();
-  const url = `api.giphy.com/v1/gifs/search?q=${userSearch}&api_key=${process.env.API_KEY}`
+  const url = `http://api.giphy.com/v1/gifs/search?q=${userSearch}&api_key=${process.env.API_KEY}`
 
   request.addEventListener('loadend', function() {
     const response = JSON.parse(this.responseText);
     if (this.status === 200) {
-      console.log(response.data[2]);
+      displayPic(response);
     }
   });
 
   request.open("GET", url, true);
   request.send();
 
+}
+
+function displayPic(apiResponse) {
+  let image = document.querySelector('#pic');
+  let image2 = document.querySelector('#pic2');
+  image.setAttribute("src", `${apiResponse.data[0].images.original.url}`);
+  image.setAttribute("alt", `${apiResponse.data[0].title}`);
+  image2.setAttribute("src", `${apiResponse.data[2].images.original.url}`);
+  image2.setAttribute("alt", `${apiResponse.data[2].title}`);
 }
 
 function handleFormSubmission(event) {
