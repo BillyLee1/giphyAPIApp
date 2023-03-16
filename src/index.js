d5461f5f7cf96b3881a32ab158960b1bbc3849e0
Billy Lee <codingbillylee@gmail.com>
2023-03-15 20:03:40 -0700

    Add js for search function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+
+function fetchGiph(userSearch) {
+  let request = new XMLHttpRequest();
+  const url = `api.giphy.com/v1/gifs/search?q=${userSearch}&api_key=${process.env.API_KEY}`
+
+  request.addEventListener('loadend', function() {
+    const response = JSON.parse(this.responseText);
+    if (this.status === 200) {
+      console.log(response.data[2]);
+    }
+  });
+
+  request.open("GET", url, true);
+  request.send();
+
+}
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  let userSearch = document.querySelector("#search").value;
+  document.querySelector("#search").value = null;
+  fetchGiph(userSearch);
+}
+
+window.addEventListener("load", function() {
+  let search = document.querySelector('form');
+  search.addEventListener("submit", handleFormSubmission)
+});
